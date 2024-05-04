41608156ed09dc3365253248e7d895fb04a105eb
JohnAzizJA <johnazizja10@gmail.com>
2024-05-04 13:27:08 +0300

    Recipe & Nutrions classes

---
+++ NutritionalFacts.java
@@ -0,0 +1,46 @@
+public class NutritionalFacts {
+    private int calories;
+    private int protein;
+    private int carbs;
+    private int fat;
+
+    public NutritionalFacts(int calories, int protein, int carbs, int fat) {
+        this.calories = calories;
+        this.protein = protein;
+        this.carbs = carbs;
+        this.fat = fat;
+    }
+
+    // Getters and setters
+    public int getCalories() {
+        return calories;
+    }
+
+    public void setCalories(int calories) {
+        this.calories = calories;
+    }
+
+    public int getProtein() {
+        return protein;
+    }
+
+    public void setProtein(int protein) {
+        this.protein = protein;
+    }
+
+    public int getCarbs() {
+        return carbs;
+    }
+
+    public void setCarbs(int carbs) {
+        this.carbs = carbs;
+    }
+
+    public int getFat() {
+        return fat;
+    }
+
+    public void setFat(int fat) {
+        this.fat = fat;
+    }
+}
