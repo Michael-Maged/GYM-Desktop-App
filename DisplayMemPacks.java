716b95b5dc9b1bc7d110ce56c7a7c90c88b94ff1
Mohamed-Dahy <163175166+Mohamed-Dahy@users.noreply.github.com>
2024-05-05 19:06:06 +0300

    DisplayMemPacks.java

--- DisplayMemPacks.java
+++ DisplayMemPacks.java
@@ -1,9 +1,34 @@
-import java.util.List;
-
 import usecase1.MemPackage;
 
-class DisplayMemPacks{
-    private List<MemPackage> packages = List.of();
+import java.util.ArrayList;
 
-    //TODO: use javafx to display data
+
+public class DisplayMemPacks {
+//    MemPackage memPackage = new MemPackage () ; 
+    public void Displaying (MemPackage memPackage) {
+        // for the first displaying 
+        
+        System.out.println("Package name is : " + memPackage.getPackName() );
+        
+        System.out.println("Duration : " + memPackage.getDuration());
 }
+    public void Displayclasses (ArrayList<String> classes ){
+//        ArrayList<String> classess = new  ArrayList<String> () ; 
+System.out.println("The classes in the package is : ");  
+//for (int i = 0; i < classes.size(); i++)
+for (  String i   : classes ){
+    System.out.println(i); 
+}
+        
+    } 
+        public void Displayfacilities  (ArrayList<String> facilities  ){
+
+System.out.println("The facilities  in the package is : ");  
+
+for (  String i   : facilities  ){
+    System.out.println(i); 
+}
+        
+    }
+    
+    }
