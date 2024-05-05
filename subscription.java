c8f8202c642a47f4845ba4e863aa221d4a50961a
Mohamed-Dahy <163175166+Mohamed-Dahy@users.noreply.github.com>
2024-05-05 20:08:51 +0300

    subscription.java

---
+++ subscription.java
@@ -0,0 +1,47 @@
+
+public class Subscription {
+    private String type ; 
+    private double  price ; 
+    private String method ;
+    private boolean confirmation ; 
+     
+    public Subscription (String type , double price  , String method ) {
+        this.type = type ; 
+        this.price =  price  ; 
+        this.method = method ; 
+        this.confirmation = false ; 
+    }
+    
+    
+    public void setType (String type){
+        this.type = type ; 
+    }
+    public void setPrice (double  price) {
+        this.price = price  ; 
+        
+    }
+    public void setmethod(String method){
+        this.method = method ; 
+        
+    }
+    public String getType(){
+        return type ; 
+    }
+    public double getprice(){
+        return price ; 
+        
+    }
+    public String getmethod(){
+        return method ; 
+        
+    }
+    
+/*    public boolean isConfirmed (){
+        return confirmation ; 
+    } */
+    
+    
+
+    
+
+}
